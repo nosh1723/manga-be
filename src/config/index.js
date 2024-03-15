4a495419ba2cf8ce8b9caa2baf7c66061067380f
nosh1723 <0702201088abc@gmail.com>
2024-03-16 00:13:29 +0700

    ...

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 const connect = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/f8_education_dev");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("connect db successfully");
   } catch (error) {
     console.log("error: ", error);
